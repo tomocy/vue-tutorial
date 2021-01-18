928f66b6151ccb7e52cde767e464a82637ea9336
tomocy <tomocy.dev@gmail.com>
2021-01-19 00:29:23 +0900

    Form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,183 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello container">
+    <form
+      @submit.prevent="onSubmit"
+      @keydown="form.errors.clear($event.target.name)"
+    >
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input name="name" class="input" type="text" v-model="form.name" />
+        </div>
+        <p
+          class="help is-danger"
+          v-if="form.errors.doHave('name')"
+          v-text="form.errors.get('name')"
+        ></p>
+      </div>
+      <div class="field">
+        <label class="label">Description</label>
+        <div class="control">
+          <input
+            name="description"
+            class="input"
+            type="text"
+            v-model="form.description"
+          />
+        </div>
+        <p
+          class="help is-danger"
+          v-if="form.errors.doHave('description')"
+          v-text="form.errors.get('description')"
+        ></p>
+      </div>
+      <div class="control">
+        <button
+          :disabled="!form.canSubmit()"
+          :class="[{ 'is-loading': form.isSubmitting }, 'button', 'is-link']"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+class Errors {
+  constructor() {
+    this.errors = {};
+  }
+
+  doHave(field) {
+    return field in this.errors;
+  }
+
+  doHaveAny() {
+    return Object.keys(this.errors).length > 0;
+  }
+
+  get(field) {
+    return this.errors[field] ? this.errors[field] : null;
+  }
+
+  set(errors) {
+    this.errors = errors;
+  }
+
+  clear(field) {
+    if (field) {
+      delete this.errors[field];
+      return;
+    }
+
+    this.errors = {};
   }
 }
+
+class Form {
+  constructor(data) {
+    this.isSubmitting = false;
+
+    this.originalData = data;
+
+    for (const field in data) {
+      this[field] = data[field];
+    }
+
+    this.errors = new Errors();
+  }
+
+  canSubmit() {
+    return this.isValid() && !this.isSubmitting && !this.errors.doHaveAny();
+  }
+
+  submit() {
+    return new Promise((resolve, reject) => {
+      if (!this.validate()) {
+        reject(this.errors);
+        return;
+      }
+
+      this.isSubmitting = true;
+
+      setTimeout(() => {
+        this.isSubmitting = false;
+        const data = this.data();
+        this.onSuccess();
+        resolve(data);
+      }, 2000);
+    });
+  }
+
+  isValid() {
+    for (const field in this.originalData) {
+      if (this[field] === "") {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  validate() {
+    let valid = true;
+    let errors = {};
+
+    for (const field in this.originalData) {
+      if (this[field] === "") {
+        errors[field] = `The ${field} is empty`;
+        valid = false;
+      }
+    }
+
+    this.errors.set(errors);
+    return valid;
+  }
+
+  onSuccess() {
+    alert("Success");
+    this.reset();
+  }
+
+  reset() {
+    this.isSubmitting = false;
+
+    for (const field in this.originalData) {
+      this[field] = null;
+    }
+
+    this.errors.clear();
+  }
+
+  data() {
+    let data = {};
+
+    for (const field in this.originalData) {
+      data[field] = this[field];
+    }
+
+    return data;
+  }
+}
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      form: new Form({
+        name: "",
+        description: "",
+      }),
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.form.submit().then(console.log).catch(console.log);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
